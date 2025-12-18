fd3a3d48184c2d6af1d47f0631dd22cf222908a0
malekazlitne <8k6n5nq594@privaterelay.appleid.com>
2025-12-18 02:34:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {  
+    const menu = document.querySelector('.list');  
+    const menuBtn = document.querySelector('.menu-btn');  
+    menu.classList.toggle('active'); 
+    menuBtn.classList.toggle('active');
+    
+    
+}
+
+   
